511d5dd54d76596d2863ffaf7390da0412722c27
Emeka-web-dev <chukwurex042@gmail.com>
2024-01-27 08:57:55 -0800

    third commit

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -9,7 +9,7 @@ export const Tooltip = ({ name, children, ...others }: TooltipProps) => {
   return (
     <div className="w-full relative group flex justify-center items-center">
       <div className="w-full">{children}</div>
-      <div className="absolute ml-2 left-[100%] hidden lg:group-hover:flex">
+      <div className="absolute ml-2 left-[100%] hidden lg:group-hover:flex z-50">
         <p className="border bg-gray-700 text-white px-2 rounded-lg">{name}</p>
       </div>
     </div>
